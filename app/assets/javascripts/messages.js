1410315eaf8f93567a0bf58482f53f8f8f6e6988
iinotomoya <iinotomoya@yahoo.co.jp>
2019-12-05 13:34:57 +0900

    途中

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,4 +1,14 @@
 $(function(){
+
+  function buildMessage(message){
+    var html = `<div class="main_chat__contents__content__coment">
+                ${message.message}
+                </div>`
+    return html
+  }
+
+
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +21,20 @@ $(function(){
       processData: false,
       contentType: false
     })
+
+    .done(function(message){
+      var html = buildMessage(message)
+      $('.main_chat__contents').append(html)
+      $('#message_message').val('')
+      $(".form__mask__button").removeAttr("disabled");
+
+    })
+    
+    .fail(function(){
+      alert("エラー");
+    })
+
+    .always(() => {
+      });
   })
 })
