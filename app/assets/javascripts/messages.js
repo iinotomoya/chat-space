22ab72aa83ad617b10f8246ea068264d75addf65
iinotomoya <iinotomoya@yahoo.co.jp>
2019-12-09 18:26:20 +0900

    自動更新

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,125 +1,75 @@
 $(function(){
-//////////////////////////////////////////////////////////////////////////////////////////////////////
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    let element = document.getElementById('element');
-    console.log(element)
-    var last_message_id = element.dataset.id  
-
+    var last_message_id = $('.main_chat__contents__content').last().data('id');
     $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-      url: "/groups/last_message_id/api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
+      url: "api/messages",
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      console.log('success');
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildMessage(message)
+      });
+      $('.main_chat__contents').append(insertHTML);
+      $('.main_chat__contents').animate({ scrollTop: $('.main_chat__contents')[0].scrollHeight});
     })
+
     .fail(function() {
       console.log('error');
-    });
+    })
   };
 
+  var buildMessage = function(message) {
+    if (message.message && message.image) {
+      var html = `<div class="main_chat__contents__content" data-id = ${message.id}>
+                    <div class="main_chat__contents__content__nickname">
+                      ${message.user_name}
+                      <div class="main_chat__contents__content__nickname--today">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                        ${message.message}
+                      </p>
+                    </div>
+                  </div>`
+      return html;
+    } else if (message.message) {
+      var html = `<div class="main_chat__contents__content" data-id = ${message.id}>
+                    <div class="main_chat__contents__content__nickname">
+                      ${message.user_name}
+                      <div class="main_chat__contents__content__nickname--today">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                        ${message.message}
+                      </p>
+                    </div>
+                  </div>`
 
-  ///////////////////////////////////////////////////////////////////////////////////////////////////
-  function buildMessage(message){ 
-
-    if (message.content && message.image) {
-      var html = `<div class="main_chat__contents__content" data-id = ${message.id} id = "element">
-      <div class="main_chat__contents__content__nickname">
-        ${message.user_name}
-        <div class="main_chat__contents__content__nickname--today">
-          ${message.created_at}
-        </div>
-      </div>
-
-      <div class="main_chat__contents__content__coment">
-        ${message.message}
-      </div>
-    </div>`
-
-    } else if (message.content) {
-
-
-    } else if (message.image) {
-
-
+      return html;
+    } else {
+      var html = `<div class="main_chat__contents__content" data-id = ${message.id}>
+                    <div class="main_chat__contents__content__nickname">
+                      ${message.user_name}
+                      <div class="main_chat__contents__content__nickname--today">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                        ${message.message}
+                      </p>
+                    </div>
+                  </div>`
+      return html;
     };
-
-    // var html = `<div class="main_chat__contents__content" data-id = ${message.id} id = "element">
-    //               <div class="main_chat__contents__content__nickname">
-    //                 ${message.user_name}
-    //                 <div class="main_chat__contents__content__nickname--today">
-    //                   ${message.created_at}
-    //                 </div>
-    //               </div>
-
-    //               <div class="main_chat__contents__content__coment">
-    //                 ${message.message}
-    //               </div>
-    //             </div>`
-    return html;
   };
-
-
-
-    // var buildHTML = function(message) {
-    //   if (message.content && message.image) {
-    //     //data-idが反映されるようにしている
-    //     var html = `<div class="message" data-message_id=` + message.id + `>` +
-    //       `<div class="upper-message">` +
-    //         `<div class="upper-message__user-name">` +
-    //           message.user_name +
-    //         `</div>` +
-    //         `<div class="upper-message__date">` +
-    //           message.created_at +
-    //         `</div>` +
-    //       `</div>` +
-    //       `<div class="lower-message">` +
-    //         `<p class="lower-message__content">` +
-    //           message.content +
-    //         `</p>` +
-    //         `<img src="` + message.image + `" class="lower-message__image" >` +
-    //       `</div>` +
-    //     `</div>`
-    //   } else if (message.content) {
-    //     //同様に、data-idが反映されるようにしている
-    //     var html = `<div class="message" data-message_id=` + message.id + `>` +
-    //       `<div class="upper-message">` +
-    //         `<div class="upper-message__user-name">` +
-    //           message.user_name +
-    //         `</div>` +
-    //         `<div class="upper-message__date">` +
-    //           message.created_at +
-    //         `</div>` +
-    //       `</div>` +
-    //       `<div class="lower-message">` +
-    //         `<p class="lower-message__content">` +
-    //           message.content +
-    //         `</p>` +
-    //       `</div>` +
-    //     `</div>`
-    //   } else if (message.image) {
-    //     //同様に、data-idが反映されるようにしている
-    //     var html = `<div class="message" data-message_id=` + message.id + `>` +
-    //       `<div class="upper-message">` +
-    //         `<div class="upper-message__user-name">` +
-    //           message.user_name +
-    //         `</div>` +
-    //         `<div class="upper-message__date">` +
-    //           message.created_at +
-    //         `</div>` +
-    //       `</div>` +
-    //       `<div class="lower-message">` +
-    //         `<img src="` + message.image + `" class="lower-message__image" >` +
-    //       `</div>` +
-    //     `</div>`
-      // 
-
-
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "groups#index"
-
+  resources :users, only: [:index, :create, :new, :edit, :update, :delete]
   resources :groups, only: [:index, :create, :new, :edit, :update] do
     resources :messages, only: [:index, :create]
-  end
-  resources :users, only: [:index, :create, :new, :edit, :update, :delete]
-
-  namespace :api do
-    resources :messages, only: :index, defaults: { format: 'json' }
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
 end
